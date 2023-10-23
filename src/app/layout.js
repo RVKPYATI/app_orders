ef0d3660c59c881e27716e23f3df5b836a08e0d6
alsam2009 <den.fstack@ya.ru>
2023-10-23 18:34:09 +0300

    add folder's structure + packages + TW config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,32 @@
-import { Inter } from "next/font/google";
+import { Inter, Irish_Grover } from "next/font/google";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin", "cyrillic"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const irish = Irish_Grover({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  variable: "--font-irish",
+});
 
 export const metadata = {
-  title: "Our Next App",
-  description: "Dont't forget change this!",
+  title: "Заявки",
+  description: "Приложение для работы диспетчера с заявками клиентов",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${irish.variable}`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inter: ["var(--font-inter)", "ui-sans-serif"],
+        irish: ["var(--font-irish)", "ui-sans-serif"],
+      },
+      colors: {
+        baseColor: "#f7f7f7",
+        primary: "#273B76",
+        primaryHover: "#0B1F5A",
+        redBadge: "#D61010",
+        badgeLight: "#F68181",
+        badgeLightHover: "#CE5959",
+      },
+      animation: {
+        rotation: "rotation 5s linear infinite",
+      },
+      keyframes: {
+        rotation: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@designbycode/tailwindcss-text-shadow")({
+      shadowColor: "rgba(0, 0, 0, 0.9)",
+      shadowBlur: "3px",
+      shadowOffsetX: "1px",
+      shadowOffsetY: "1px",
+      experimental: true,
+    }),
+  ],
+};
